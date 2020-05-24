1370a7185a6070da655c4dc2daa6150ea5600e21
Mounir Afifi <mounir.afifi@intern.promatis.de>
2020-05-24 15:13:00 +0200

    added docstring and comments to impl.py

--- impl.py
+++ impl.py
@@ -3,11 +3,20 @@ from sudoku import *
 import numpy as np
 import subprocess, os
 
-
 if __name__ == '__main__':
+    """
+    Implementation of Sudoku for Windows. Not
+    tested for Linux.
+    - Creates and generates a random Sudoku
+    board.
+    - Draws the board and exports to postscript
+    file.
+    - Converts postscript file to PDF.
+    - If the conversion fails an error is printed.
+    """
     sudoku = Sudoku(None)
     sudoku.build_random_game(9)
-    #print(list(sudoku.board))
+    
     drawing = Drawing(sudoku.board.tolist())
     filename = drawing.draw_sudoku('')
 
@@ -15,11 +24,13 @@ if __name__ == '__main__':
     
     pdf_name = filename[:-2]+'pdf'
 
-
+    #replace PS2_PATH with path to Ghostscript lib folder
     PS2_PATH = 'C:\\Program Files\\gs\\gs9.52\\lib'
     os.environ["PATH"] += os.pathsep + PS2_PATH
+    
     instruction = 'ps2pdf "'+os.getcwd()+'\\"'+filename+' "'+os.getcwd()+'\\'+pdf_name+'"'
     res = subprocess.Popen(instruction, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
     # Wait for the process end and print error in case of failure 
     if res.wait() != 0:
         output, error = res.communicate()
