460f6c8b0702c1dafac559ccad7557f4ea3ebb22
Mounir Afifi <mounir.afifi@intern.promatis.de>
2020-05-24 16:46:14 +0200

    adding docstrings and comments to draw and sudoku class scripts

--- draw.py
+++ draw.py
@@ -1,30 +1,48 @@
 import turtle
 import os
 from datetime import datetime
-import numpy as np
 
+#size of the square side
 SQUARE_SIDE = 75
 
 DEF_WINDOW_SIZE_X = 1000
 DEF_WINDOW_SIZE_Y = 800
 
 #display coordinate corrections for numbers
+#these were obtained by trial and error on
+#multiple board sizes
 CORRX = 25
 CORRY = 60
 
 class Drawing():
-
+    """
+    Class that handles drawing a Sudoku
+    board on canvas, and exporting it to
+    postscript file.
+    """
     def __init__(self,values):
-        self.grid = turtle.Turtle()
+        """
+        Sets the values of the Sudoku cells.
+        Sets different parameters of the canvas
+        window, and the drawing.
+
+        --Input:
+        - values: list of lists containing 
+        """
+        
+        self.grid = turtle.Turtle() #grid object
         self.values = values  #list of lists, matrix
 
         self.board_size = len(self.values)
-        
+
+        #window size
         self.WINDOW_SIZE_X = DEF_WINDOW_SIZE_X*(self.board_size//10+1)
         self.WINDOW_SIZE_Y = DEF_WINDOW_SIZE_Y*(self.board_size//10+1)
-        self.font_size = max(25//(self.board_size//10+1),8)
+
+        #font size for numbers
+        self.font_size = max(25//(self.board_size//10+1),6)
         
-        
+        #Canvas and drawing properties
         turtle.setworldcoordinates(-1, -1, self.WINDOW_SIZE_X, self.WINDOW_SIZE_Y)
         self.grid.home()
         turtle.delay(0)
@@ -35,11 +53,28 @@ class Drawing():
         self.posy = (self.WINDOW_SIZE_Y - SQUARE_SIDE*len(self.values)) // 2
 
     def square(self,side):
+        """
+        Draws square on the canvas.
+
+        -- Input:
+        - side: side of the square in canvas units.
+        The unit is determined by the WINDOW_SIZE
+        parameters.
+        """
         for i in range(4):
             self.grid.forward(side)
             self.grid.left(90)
 
     def row(self,n,side):
+        """
+        Draws a raw of squares on the canvas.
+
+        -- Input:
+        - n: number of squares
+        - side: side of each square in canvas units.
+        The unit is determined by the WINDOW_SIZE
+        parameters.
+        """
         for i in range(n):
             self.square(side)
             self.grid.forward(side)
@@ -50,6 +85,15 @@ class Drawing():
         self.grid.pendown()
 
     def row_of_rows(self,n,side):
+        """
+        Draws a row of rows on the canvas.
+
+        -- Input:
+        - n: number of rows and squares in each row.
+        - side: side of each square in canvas units.
+        The unit is determined by the WINDOW_SIZE
+        parameters.
+        """
         self.grid.pendown()
         for i in range(n):
             self.row(n,side)
@@ -66,6 +110,21 @@ class Drawing():
 
     
     def draw_sudoku(self,filename):
+        """
+        Draws the grid on the canvas, and writes
+        the numbers in their respective cells.
+        The filename is returned so the file can be
+        converted to another format.
+
+        --Input:
+        - filename: name of the Postscript file to be
+        exported.
+
+        --Output:
+        - filename: the actual file name that was used.
+        If no name was provided, a default name is built.
+        """
+        
         side = SQUARE_SIDE
         self.grid.penup()
         self.grid.goto(self.posx,self.posy)
@@ -96,7 +155,12 @@ class Drawing():
 
 
 if __name__ == '__main__':
-   
+    """
+    Test implementation for Drawing class.
+    """
+    
+    import numpy as np
+    
     lis =   [[0,1,4,0,0,5,0,0,7],
              [0,3,0,0,2,0,8,0,0],
              [0,0,2,1,0,3,0,0,0],
@@ -117,7 +181,7 @@ if __name__ == '__main__':
              [0,0,0,0,4,0,0,1]]
 
     #testing correct display with different grid sizes
-    size = 18
+    size = 32
     ones = list(np.ones([size, size], dtype = int))
 
     drawing = Drawing(ones)
--- sudoku.py
+++ sudoku.py
@@ -4,18 +4,53 @@ from random import randint
 
 
 class Sudoku():
+    """
+    Class that handles the creation
+    and modification of a Sudoku object.
+    """
     def __init__(self,board):
+        """
+        Constructor. Intitializes the board
+
+        -- Input:
+        - board: NumPy matrix that represents
+        a Sudoku board. Empty values are set to
+        zero.
+
+        Attributes:
+        -- board: already explained
+        -- solutions: number of solutions that
+        were found when solving the Sudoku board
+        (see solve function).
+        """
         self.board = board
         self.solutions = 0
 
 
     def __repr__(self):
+        """
+        Returns a string representation of a Sudoku
+        object, which is simply the string form of
+        the board attribute.
+        """
         return str(matrix(self.board))
 
     def __str__(self):
+        """
+        Returns a string representation of a Sudoku
+        object, which is simply the string form of
+        the board attribute.
+        """
         return str(matrix(self.board))
 
     def is_perf_square_board(self):
+        """
+        Tests if the the board size is a perfect
+        square.
+
+        Returns the square root if true, and 0
+        otherwise.
+        """
         size = self.get_board_size()
         sq   = floor(sqrt(size))
 
@@ -25,9 +60,30 @@ class Sudoku():
             return 0
 
     def get_board_size(self):
+        """
+        Returns board size.
+        """
         return len(self.board)
 
     def number_fits(self,i,j,x):
+        """
+        Checks if a given number fits in a cell.
+        A number fits in a cell, if and only if
+        it does not violate Sudoku rules, that is,
+        it's not repeated in the same row, same
+        column, or the same square (if the board
+        size is a perfect square).
+        Returns a boolean value.
+
+        -- Input:
+        - i: row number
+        - j: column number
+        - x: value to check
+
+        -- Output:
+        - True: x fits in the cell (i,j)
+        - False: otherwise
+        """
         size = self.get_board_size()
         if x < 0 or x > size:
             return False
@@ -38,6 +94,9 @@ class Sudoku():
             if self.board[e,j] == x:
                 return False
 
+        #the rule of checking the sub-squares
+        #is only enabled for board sizes that
+        #are perfect squares, e.g. 4, 9, 16, etc
         sq = self.is_perf_square_board()
         if sq!=0:
             posi = (i//sq)*sq
@@ -52,6 +111,15 @@ class Sudoku():
         
 
     def solve(self):
+        """
+        Recursive function that checks each
+        empty cell for possible values, until
+        the Sudoku puzzle is solved, or no
+        solutions are found. Prints all found
+        solutions.
+        Increments the attribute solutions as
+        more solutions are found.
+        """
         size = self.get_board_size()
         for i in range(size):
             for j in range(size):
@@ -72,6 +140,17 @@ class Sudoku():
         return
 
     def pseudo_solve(self):
+        """
+        The same as solve(), except that this
+        function stops as soon as the number
+        of solutions exceeds 1 (i.e. is equal
+        to 2).
+        Does not print the solutions if any
+        are found.
+        This special implementation was created
+        to generate random Sudoku puzzle games
+        (see build_random_game).
+        """
         size = self.get_board_size()
         if self.solutions > 1:
             return
@@ -92,18 +171,37 @@ class Sudoku():
         return
 
     def build_random_board(self,size):
-        self.board = zeros([size,size],dtype = int)
+        """
+        Updates the board attribute of the current
+        object, by setting the board size, setting
+        all values to zero, and initializing some
+        cells to random values.
 
-        for k in range(size):
+        -- Input:
+        - size: size of the board, and also number
+        of cells that will be initiated.
+        """
+        
+        self.board = zeros([size,size],dtype = int)
+        k = 0
+        while k<size:
             i = randint(0,size-1)
             j = randint(0,size-1)
             x = randint(1,size)
             if self.board[i,j] == 0 and self.number_fits(i,j,x):
                 self.board[i,j] = x
-            else:
-                k-=1
+                k+=1
 
     def build_random_game(self,size):
+        """
+        Creates a random Sudoku game. Keeps checking
+        the solutions to the game and adding values,
+        until an optimal game is found, i.e. one that
+        has the least number of values but a single
+        solution.
+        -- Input:
+        - size: size of the board.
+        """
         self.build_random_board(size)
         
         while self.solutions != 1:
@@ -115,21 +213,20 @@ class Sudoku():
                 self.board[i,j] = x
                 print(self)
                 self.pseudo_solve()
-                print(self.solutions)
+                print('Number of solutions: '+str(self.solutions))
                 if self.solutions == 0:
                     self.board[i,j] = 0
                     
-
+        print('Sudoku game generated:')
         print(self)
 
-            
-             
-            
 
-    
 
 if __name__=='__main__':
-        
+    """
+    Test implementation for Sudoku class
+    and its functionalities.
+    """
     sudoku = Sudoku(matrix(([0,1,4,0],
                             [0,3,0,0],
                             [0,0,2,1],
